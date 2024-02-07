0c769260eebca7d46bc9e09969ab5dedfd7a556e
karthika M <karthikam0912@gmail.com>
2024-02-07 20:35:46 +0530

    first commit

---
+++ assets/process.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // Database credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = " ";
+    $dbname = "portfolio";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Insert data into the database
+    $sql = "INSERT INTO contacts (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Data inserted successfully
+        echo "Submitted successfully. Thank you!";
+    } else {
+        // Error in inserting data
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $conn->close();
+} else {
+    // If the form is not submitted via POST, you can redirect to the form page
+    header("Location: your_contact_page.html");
+    exit();
+}
+?>
